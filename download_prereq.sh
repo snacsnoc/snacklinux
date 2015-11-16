e7d57b05ca83348da9f38cbb1236aa92edc45c31
Easton Elliott <easton@geekness.eu>
2015-11-15 18:40:14 -0700

    Add script to download prerequisite packages

---
+++ download_prereq.sh
@@ -0,0 +1,47 @@
+#! /bin/sh
+
+# Download package prerequisites
+# Mirrors of the packages can also be found at http://snacklinux.org/tars/
+# Original script from gcc
+#
+# (C) 2010 Free Software Foundation
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
+
+LINUX=linux-4.0.5
+MUSL=musl-1.1.12
+BUSYBOX=busybox-1.23.2
+BINUTILS=binutils-2.25
+BASH=bash-4.3
+
+
+wget http://kernel.org/pub/linux/kernel/v4.x/$LINUX.tar.xz || exit 1
+tar xf $LINUX.tar.xz || exit 1
+ln -sf $LINUX linux || exit 1
+
+wget http://www.musl-libc.org/releases/$MUSL.tar.gz || exit 1
+tar xf $MUSL.tar.gz || exit 1
+ln -sf $MUSL musl || exit 1
+
+wget http://ftp.gnu.org/gnu/binutils/$BINUTILS.tar.bz2 || exit 1
+tar xjf $BINUTILS.tar.bz2 || exit 1
+ln -sf $BINUTILS binutils || exit 1
+
+wget http://www.busybox.net/downloads/$BUSYBOX.tar.bz2 || exit 1
+tar xjf $BUSYBOX.tar.bz2 || exit 1
+ln -sf $BUSYBOX busybox || exit 1
+
+wget https://ftp.gnu.org/gnu/bash/$BASH.tar.gz || exit 1
+tar xf $BASH.tar.gz || exit 1
+ln -sf $BASH bash || exit 1
