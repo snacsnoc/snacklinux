7bc4fea1c39bdf9c68349be0198bbbff38f827ea
Easton Elliott <easton@geekness.eu>
2023-01-22 23:43:15 -0700

    Add download_extra, for extra non-boot packages

--- defs.sh
+++ defs.sh
@@ -9,3 +9,4 @@ BINUTILS=binutils-2.39
 BASH=bash-5.2.15
 SYSLINUX=syslinux-6.03
 PYTHON=Python-3.11.1
+OPENSSL=openssl-1.1.1s
---
+++ download_extra.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Download package prerequisites
+# Mirrors of the packages can also be found at http://snacklinux.geekness.eu/tars/
+# Original script from gcc
+#
+# (C) 2010 Free Software Foundation
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see http://www.gnu.org/licenses/.
+
+source defs.sh
+
+wget https://www.python.org/ftp/python/$PYTHON/$PYTHON.tar.xz
+tar xvf $PYTHON.tar.xz || exit 1
+ln -sf $PYTHON.tar.xz python || exit 1
+
+wget https://www.openssl.org/source/$OPENSSL.tar.gz
+tar xvf $OPENSSL.tar.gz || exit 1
+ln -sf $OPENSSL.tar.gz openssl || exit 1
