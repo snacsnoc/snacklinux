86bd3ff6ef7cc2e99f6b82f7188965922e823c9c
Easton Elliott <easton@geekness.eu>
2023-11-13 13:41:20 -0700

    Update kernel and all utils

--- defs.sh
+++ defs.sh
@@ -2,11 +2,11 @@
 
 # Package versions
 
-LINUX=linux-6.0.12
-MUSL=musl-1.2.3
-BUSYBOX=busybox-1.35.0
-BINUTILS=binutils-2.39
-BASH=bash-5.2.15
+LINUX=linux-6.6.1
+MUSL=musl-1.2.4
+BUSYBOX=busybox-1.36.1
+BINUTILS=binutils-2.41
+BASH=bash-5.2.21
 SYSLINUX=syslinux-6.03
 PYTHON=Python-3.11.1
 OPENSSL=openssl-1.1.1s
